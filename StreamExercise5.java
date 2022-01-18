47ba143cf0cbfac0152a9e1794c74078d0fc878a
Nikhil Kaushal <kaushalnikhil99@gmail.com>
2022-01-18 15:40:11 +0530

    StreamExercise5 question 1 statement corrected

--- StreamExercise5.java
+++ StreamExercise5.java
@@ -60,7 +60,7 @@ class Transaction{
 
 // questions...........................................
 
-//1 Find all transactions in the year 2011 and sort them by value (small to high).
+//1 Find all transactions in the year 2011 and sort them by value (high to small).
 //2 What are all the unique cities where the traders work?
 //3 Find all traders from Cambridge and sort them by name.
 //4 Return a string of all traders’ names sorted alphabetically.
